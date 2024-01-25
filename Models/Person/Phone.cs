1d2d055438f589fc410465fdd68af0c8ddd61654
Daniele Tavares Lobo <danitavares.dev@gmail.com>
2024-01-25 17:07:17 -0300

    Incluindo Validação de Telefone

--- Models/Person/Phone.cs
+++ Models/Person/Phone.cs
@@ -1,14 +1,24 @@
 ﻿using AgendaLarAPI.Models.Base;
+using AgendaLarAPI.Models.Person.Validators;
+using Model = AgendaLarAPI.Models.Person;
 
 namespace AgendaLarAPI.Models.Person
 {
     public class Phone : Entity
     {
+        public const int NumberMinLength = 20;
         public const int NumberMaxLength = 20;
 
         public Guid PersonId { get; set; }
         public Person Person { get; set; }
         public PhoneType Type { get; set; }
         public string Number { get; set; }
+        public override bool IsValid => Validate();
+
+        private bool Validate()
+        {
+            Validate(this, new PhoneValidator());
+            return ValidationResult.IsValid;
+        }
     }
 }
---
+++ Models/Person/Validators/PhoneValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace AgendaLarAPI.Models.Person.Validators
+{
+    public class PhoneValidator : AbstractValidator<Phone>
+    {
+        public PhoneValidator()
+        {
+            RuleFor(x => x.Number)
+                .NotEmpty()
+                .WithMessage("O número do telefone é obrigatório.")
+                .MaximumLength(Phone.NumberMaxLength)
+                .WithMessage("O número do telefone não pode ter mais que {MaxLength} caracteres.")
+                .MinimumLength(Phone.NumberMinLength)
+                .WithMessage("O número do telefone deve ter pelo menos {MinLength} caracteres.");
+
+            RuleFor(x => x.PersonId)
+                .NotEmpty()
+                .WithMessage("O Id da pessoa é obrigatório.");
+
+            RuleFor(x => x.Type)
+                .NotEmpty()
+                .WithMessage("O tipo do telefone é obrigatório.")
+                .IsInEnum()
+                .WithMessage("O tipo do telefone é inválido.");
+        }
+    }
+}
